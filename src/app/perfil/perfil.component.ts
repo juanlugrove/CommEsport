51715ccc4d65822336ed6394ef1a7159243b3612
juanlugrove <grovejuanluis@gmail.com>
2022-06-18 21:07:26 +0200

    Edit User arreglado

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,10 +1,12 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { environment } from 'src/environments/environment';
 import { AuthService } from '../auth.service';
+import Swal from 'sweetalert2';
+import { catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-perfil',
@@ -19,23 +21,24 @@ export class PerfilComponent implements OnInit {
   seleccionadoPlatform=[false,false,false];
   seleccionadoPosicion=[false,false,false,false];
   seleccionadoPosicion2=[false,false,false,false];
+  prueba={twitter:"p2p"};
+  headers= new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer '+this.cookieService.get("key")
+  })
+  requestOptions = {headers: this.headers};
   constructor(private formBuilder: FormBuilder, private http: HttpClient, private authService: AuthService, private router: Router, private cookieService: CookieService) {
     this.url = environment.urlBase;
     this.formUser=formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required],
-      platform: ['', Validators.required]
+      platform: ['', Validators.required],
+      twitter: [''],
+      position: [''],
+      secondPosition: [''],
     });
    }
 
   ngOnInit(): void {
-    const headers= new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer '+this.cookieService.get("key")
-    })
-    const requestOptions = {headers: headers};
-
-    this.http.get(this.url+"usuarios", requestOptions)
+    this.http.get(this.url+"usuarios", this.requestOptions)
       .subscribe(
         resultado => {
           console.log(resultado);
@@ -43,11 +46,17 @@ export class PerfilComponent implements OnInit {
           this.userData=resultado;
           this.seleccionadoPlatform[this.userData.platform]=true;
           if(this.userData.position!=null){
-            this.seleccionadoPosicion[this.marcarSeleccion(this.userData.position)]=true;
+            this.seleccionadoPosicion[this.userData.position]=true;
           }
           if(this.userData.secondPosition!=null){
-            this.seleccionadoPosicion2[this.marcarSeleccion(this.userData.secondPosition)]=true;
+            this.seleccionadoPosicion2[this.userData.secondPosition]=true;
           }
+          this.formUser.patchValue({
+            twitter: this.userData.twitter,
+            position: this.userData.position,
+            secondPosition: this.userData.secondPosition,
+            platform: this.userData.platform
+          })
         }
       );
   }
@@ -59,15 +68,36 @@ export class PerfilComponent implements OnInit {
     });
   }
 
-  marcarSeleccion(posicion:string): number{
-    if(posicion=="DC"){
-      return 0;
-    } else if(posicion=="MC"){
-      return 1;
-    } else if(posicion=="DF"){
-      return 2;
-    } else {
-      return 3;
-    }
+  guardar(): void{
+    // console.log(this.formUser.get('twitter'));
+    console.log(this.formUser.value)
+    let cambioUser:any;
+    this.tengoDatos=false;
+    this.http.put(this.url+"usuarios/"+this.userData.idUser,this.formUser.value,this.requestOptions).pipe(catchError(this.errorHandler)).subscribe( r => {
+      console.log(this.formUser.value.twitter);
+      this.tengoDatos=true;
+      this.userData=r
+      Swal.fire({
+        position: 'top',
+        icon: 'success',
+        title: 'Datos guardados exitosamente',
+        showConfirmButton: false,
+        timer: 1500
+      })
+      window.location.reload();
+    }, error => {
+      console.log(error);
+      this.tengoDatos=true;
+      Swal.fire({
+        title: 'Error',
+        text: 'Ha ocurrido un error',
+        icon: 'error',
+        confirmButtonText: 'Cool'
+      })
+    });
+  }
+
+  errorHandler(error: HttpErrorResponse){
+    return throwError(error.message || "Server Error");
   }
 }
